fffdbda72f397e0117ab9f52b42e6a94e714e734
J0Hn-git <johnjoji2222@gmail.com>
2025-12-30 13:40:54 +0530

    moviecard implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -17,35 +19,71 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort-by=popularity.desc`
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+
+      if(!data.Response === 'False'){
+        setErrorMessage(data.Error || 'Failed to fetch movies.');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
     } catch (error) {
-      console.log(`Error fetching movies: ${error}`);
+      console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
-
+    fetchMovies();
   }, []);
 
   return (
     <main>
       <div className="pattern"/>
+
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="Hero Banner" />
+            <img src="heros.png" alt="Hero Banner" />
+      
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
 
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}/>
+              ))}
+            </ul>
+          )}
         </section>
       </div>
     </main>
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const MovieCard = ({movie: {title, vote_average, poster_path, release_date,
+    original_language
+}}) => {
+    return (
+        <div className="movie-card">
+            <img 
+                src={poster_path ? 
+                    `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'
+                }
+                alt={title}
+                />
+
+                <div className="mt-4">
+                    <h3>{title}</h3>
+                </div>
+
+                <div className="content">
+                    <div className="rating"></div>
+                    <img src="star.svg" alt="Star Icon" className="w-4 h-4 object-contain"/>
+                </div>
+        </div>
+
+    );
+};
+
+export default MovieCard;
